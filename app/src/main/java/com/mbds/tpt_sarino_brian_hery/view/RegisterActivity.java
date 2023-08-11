a92d0d3d43636cb85b4acf7701666e6a0250a689
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-08-11 12:06:00 +0300

    Commit initial

---
+++ app/src/main/java/com/mbds/tpt_sarino_brian_hery/view/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.mbds.tpt_sarino_brian_hery.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mbds.tpt_sarino_brian_hery.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/test/java/com/mbds/tpt_sarino_brian_hery/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.mbds.tpt_sarino_brian_hery;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
