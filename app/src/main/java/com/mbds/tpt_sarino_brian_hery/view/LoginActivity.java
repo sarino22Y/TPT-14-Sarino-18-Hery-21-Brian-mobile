406b9296410bdf817dd053b37d55be89e547ff6c
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-08-13 02:55:41 +0300

    Mise en place de fragment pour chaque fonctionnalité

--- app/src/main/java/com/mbds/tpt_sarino_brian_hery/view/LoginActivity.java
+++ app/src/main/java/com/mbds/tpt_sarino_brian_hery/view/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.mbds.tpt_sarino_brian_hery.view;
 
 import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,11 +26,16 @@ public class LoginActivity extends AppCompatActivity {
         init();
     }
 
+    FirebaseAuth firebaseAuth;
     TextView lnkRegister;
+    EditText txtEmail, txtPwd;
     private static final int RC_SIGN_IN = 123; // Un code de demande arbitraire
 
     private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
         lnkRegister = findViewById(R.id.lnkRegister);
+        txtEmail = findViewById(R.id.txtEmailLogin);
+        txtPwd = findViewById(R.id.txtPwdLogin);
         // Vérifiez si l'utilisateur est déjà connecté
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser != null) {
@@ -40,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
             startSignIn();
             Toast.makeText(this, "Vous êtes sur la page de connexion", Toast.LENGTH_SHORT).show();
         }
+        login(txtEmail.getText().toString(), txtPwd.getText().toString());
         goToRegister();
     }
 
@@ -78,6 +87,46 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+
+    /**
+     * Se connecter avec un compte existant.
+     */
+    private void login(String email, String pwd) {
+        findViewById(R.id.btnLogin).setOnClickListener(v -> {
+            if (checkFields(email, pwd)) {
+                if (firebaseAuth.createUserWithEmailAndPassword(email,pwd).isComplete()) {
+                    Toast.makeText(this, "Vous êtes connecté", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Erreur lors de la connexion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Controle des champs.
+     */
+    public boolean checkFields(String email, String pwd) {
+        boolean isValid = true;
+        if (TextUtils.isEmpty(email)) {
+            txtEmail.setError("Entrer un e-mail");
+            txtEmail.requestFocus();
+            isValid = false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            txtEmail.setError("Entrer un e-mail valide");
+            txtEmail.requestFocus();
+            isValid = false;
+        }
+        if (TextUtils.isEmpty(pwd)) {
+            txtPwd.setError("Entrer un mot de passe");
+            txtPwd.requestFocus();
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
 
     /**
      * Redirection vers la page d'inscription.
